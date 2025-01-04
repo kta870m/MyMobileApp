e30865220bf2a294dd36d5df24e4054241f7cdac
kta870m <kta87067@gmail.com>
2025-01-04 13:59:10 +0700

    first-commit

--- Week1/MyMobileApp/app/build.gradle.kts
+++ Week1/MyMobileApp/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "vn.swinburne.mymobileapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "vn.swinburne.mymobileapp"
